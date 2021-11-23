7688402525d5b8bfca5e87a353f09a6065f501f5
Antonio Eloi Fernandes Junior <antonio.junior@spread.com.br>
2021-11-22 22:40:29 -0300

    crud simples em quarkus

--- src/test/java/br/com/antonio/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.antonio;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
